e0e5a1ffb818707112c4ab02e3cea6f8d02724cd
dmitry <dyivanov32@gmail.com>
2026-02-19 03:19:50 +0200

    sub commit

--- app/(auth)/Signup/page.tsx
+++ app/(auth)/Signup/page.tsx
@@ -2,31 +2,7 @@ import Link from "next/link";
 import React from "react";
 
 function page() {
-  return (
-    <form className="flex flex-col">
-      <h2 className="text-3xl font-semibold pb-2">Sign up</h2>
-      <div className="flex flex-col gap-2 pb-2">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          className="border rounded-sm shadow-lg focus:outline-0 py-1 px-3"
-        />
-      </div>
-      <div className="flex flex-col gap-2">
-        <label htmlFor="password">Passowrd</label>
-        <input type="password" id="password" />
-      </div>
-      <div className="flex flex-col gap-2">
-        <label htmlFor="rPassword">Repeat Password</label>
-        <input type="password" id="rPassword" />
-      </div>
-
-      <Link href={"/Signin"} className="text-sm underline">
-        Already have an account? Sign in
-      </Link>
-    </form>
-  );
+  return <></>;
 }
 
 export default page;

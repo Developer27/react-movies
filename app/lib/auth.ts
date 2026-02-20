b810aa8495707cdd65eeb3e71f7fc8f01a147c35
dmitry <dyivanov32@gmail.com>
2026-02-20 23:02:05 +0200

    updated db connection

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,20 +1,21 @@
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import GitHub from "next-auth/providers/github"
+import prisma from "./db"
 export const { auth, handlers, signIn } = NextAuth({ providers: [GitHub, Credentials({
   credentials: {
     email: {},
     password: {}
   },
   
-  authorize: async (credentials) => {
-    const email = "admin@admin.com";
-    const password = "1234";
-    console.log(credentials.email, credentials.password)
-    if(credentials.email === email && credentials.password === password) {
-      return {email, password}
-    } else {
-      throw new Error("Invalid credentials");
-    }
-  }
+  // authorize: async (credentials) => {
+  //   const user = await prisma.user.findFirst({
+  //     where: { email: credentials.email, password: credentials.password},
+  //   })
+
+  //   if(!user) {
+  //     throw new Error("Invalid credentials.");
+  //   }
+  //   return user;
+  // }
 })] }) 
